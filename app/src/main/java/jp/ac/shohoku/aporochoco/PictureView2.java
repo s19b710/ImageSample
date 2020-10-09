978b500554c898b1ea3ad5d8d08945113eeca7f4
s19B710 <s19b710@shohoku.ac.jp>
2020-10-09 11:57:26 +0900

    変わったかな？

---
+++ app/src/main/java/jp/ac/shohoku/aporochoco/PictureView2.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.aporochoco;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PictureView2 extends View {
+    public PictureView2(Context context) {
+        super(context);
+    }
+
+    public PictureView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
