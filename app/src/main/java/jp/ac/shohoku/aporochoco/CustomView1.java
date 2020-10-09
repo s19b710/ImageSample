ea2e8b60f63e9aa6d06e06c410bac229ec33a0e4
s19B710 <s19b710@shohoku.ac.jp>
2020-10-09 11:13:42 +0900

    追加したよ

---
+++ app/src/main/java/jp/ac/shohoku/aporochoco/CustomView1.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.aporochoco;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView1 extends View {
+    public CustomView1(Context context) {
+        super(context);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/jp/ac/shohoku/aporochoco/CustomView2.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.aporochoco;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView2 extends View {
+    public CustomView2(Context context) {
+        super(context);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
